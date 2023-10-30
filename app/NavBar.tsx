9a1b8eacf1069fe2b265edccab8f0a530994b52e
Rolando L Gonzales <rolandolloyd@gmail.com>
2023-10-30 12:56:10 -0400

    Refactor the NavBar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -3,29 +3,20 @@ import React from "react";
 import { SiPivotaltracker } from "react-icons/si";
 
 const NavBar = () => {
+   const links = [
+      { label: "Dashboard", href: "/" },
+      { label: "Issues", href: "/issues" },
+   ]
 	return (
 		<nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center bg-fuchsia-50">
-			{/* display: flex
-          margin: 0, 1rem
-          border-bottom: 1px
-          margin-bottom: 5px
-          padding: 0, 1.25rem
-          height: 3.5rem
-          align-items: center */}
 			<Link href="/">
 				<SiPivotaltracker className="w-8 h-8" />
 			</Link>
 			<ul className="flex space-x-6">
-				<li>
-					{" "}
-					<Link className="text-zinc-500 hover:text-zinc-800 transition-color" href="/">
-						Dashboard
-					</Link>
-				</li>
-				<li>
-					{" "}
-					<Link href="/">Issues</Link>
-				</li>
+            { links.map(link => <Link
+               key={link.href}
+               className="text-zinc-500 hover:text-zinc-800 transition-color"
+               href={link.href}> {link.label} </Link> )}
 			</ul>
 		</nav>
 	);
