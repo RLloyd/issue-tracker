155377c164fbc6b94b1c0fca0552d30c48626b9a
Rolando L Gonzales <rolandolloyd@gmail.com>
2023-10-30 14:43:20 -0400

    Styling the Active Link

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,22 +1,31 @@
+'use client';
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 import { SiPivotaltracker } from "react-icons/si";
 
 const NavBar = () => {
+   const currentPath = usePathname();
+   // console.log(currentPath);
+
    const links = [
       { label: "Dashboard", href: "/" },
       { label: "Issues", href: "/issues" },
    ]
 	return (
 		<nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center bg-fuchsia-50">
-			<Link href="/">
-				<SiPivotaltracker className="w-8 h-8" />
-			</Link>
+
+         {/* Logo */}
+         <Link href="/"><SiPivotaltracker className="w-8 h-8" /></Link>
+
+         {/* Menu List */}
 			<ul className="flex space-x-6">
-            { links.map(link => <Link
-               key={link.href}
-               className="text-zinc-500 hover:text-zinc-800 transition-color"
-               href={link.href}> {link.label} </Link> )}
+            { links.map(link =>
+               <Link key={link.href}
+                  className={`${link.href === currentPath ? 'text-red-900' : 'text-zinc-500'}  hover:text-zinc-800 transition-color`}
+                  href={link.href}> {link.label} </Link> )}
+               {/* <Link key={id} className="styles" href={link} */}
 			</ul>
 		</nav>
 	);
