7d0bb9f0a55bea927715b234f755ce237de7d95d
Rolando L Gonzales <rolandolloyd@gmail.com>
2023-11-12 04:49:45 -0500

    Create the issue model

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 		<html lang="en">
 			<body className={inter.className}>
 				<NavBar />
-				<main>{children}</main>
+				<main className="p-5">{children}</main>
 			</body>
 		</html>
 	);
