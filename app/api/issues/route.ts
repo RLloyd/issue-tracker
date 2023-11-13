03933baaa2e621e5c3d6e213b7d7e54471bcb2ec
Rolando L Gonzales <rolandolloyd@gmail.com>
2023-11-13 06:07:40 -0500

    Build an API for creating issue

---
+++ app/api/issues/route.ts
@@ -0,0 +1,26 @@
+// Issues route file
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+import prisma from "@/prisma/client";
+
+// for validating data using zod
+const createIssueSchema = z.object({
+   // only validating these fields. other fields have default values
+   title: z.string().min(1).max(255),
+   description: z.string().min(1)
+})
+
+export async function POST(request: NextRequest) {
+   const body = await request.json();
+   const validation = createIssueSchema.safeParse(body);
+   if(!validation.success)
+      return NextResponse.json(validation.error.errors, {status: 400 }) //400=bad data
+
+   // otherwise: Store in the db. Create a prisma new issue schema but first create a new prisma client
+   const newIssue = await prisma.issue.create({
+      data: { title: body.title, description: body.description }
+   });
+
+   return NextResponse.json(newIssue, {status: 201 }); //object is created
+
+}
