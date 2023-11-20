e55b7ca5622b3884da18776c15b59474343ae1a4
Rolando L Gonzales <rolandolloyd@gmail.com>
2023-11-20 06:03:27 -0500

    Show the issues: RadixUI Table & Responsiveness

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,11 +1,47 @@
 import React from "react";
-import { Button } from "@radix-ui/themes";
+import { Button, Table } from "@radix-ui/themes";
 import Link  from 'next/link';
+import prisma from "@/prisma/client";
+import { SiPivotaltracker } from "react-icons/si";
+import { HiPlusCircle } from "react-icons/hi";
 
-const IssuesPage = () => {
+const IssuesPage = async () => {
+   const issues = await prisma.issue.findMany(); //find all our issues
 	return (
 		<div>
-			<Button><Link href="/issues/new">New Issue</Link></Button>
+         <div className="gdBtn-styl-1 mb-5">
+            <Button>
+               <HiPlusCircle className="w-5 h-5" />
+               <Link href="/issues/new">New Issue </Link>
+            </Button>
+         </div>
+
+
+         {/* Table container from RadixUI */}
+         <Table.Root variant="surface">
+            {/* Table Header */}
+            <Table.Header>
+               <Table.Row>
+                  <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
+                  <Table.ColumnHeaderCell className="hidden md:table-cell">Status</Table.ColumnHeaderCell> {/* hide this header below md */}
+                  <Table.ColumnHeaderCell className="hidden md:table-cell">Created</Table.ColumnHeaderCell> {/* hide this header below md */}
+               </Table.Row>
+            </Table.Header>
+            {/* Table Body Section */}
+            <Table.Body>
+               {issues.map(issue => (
+                  <Table.Row key={issue.id}>
+                     <Table.Cell>
+                        {issue.title}
+                        {/* in mobile: show status in same column as title, but hidden on md */}
+                        <div className='block md:hidden'>{issue.status}</div>
+                     </Table.Cell>
+                     <Table.Cell className="hidden md:table-cell">{issue.status}</Table.Cell>
+                     <Table.Cell className="hidden md:table-cell">{issue.createdAt.toDateString()}</Table.Cell>
+                  </Table.Row>
+               ))}
+            </Table.Body>
+         </Table.Root>
 		</div>
 	);
 };
