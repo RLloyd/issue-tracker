606564bce9ffa537a9b6322a7d6ea49338bb3d99
Rolando L Gonzales <rolandolloyd@gmail.com>
2023-11-23 05:12:56 -0500

    Add the edit button

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,11 +1,14 @@
 import prisma from '@/prisma/client'
-import { Card, Flex, Heading } from '@radix-ui/themes';
+import { Card, Flex, Grid, Heading, Box } from '@radix-ui/themes';
 import { notFound } from 'next/navigation';
 import React from 'react'
 import IssueStatusBadge from '../../components/IssueStatusBadge';
 import { Text } from '@radix-ui/themes';
 import ReactMarkdown from 'react-markdown';
 import delay from 'delay';
+import { Button } from '@radix-ui/themes';
+import { Pencil2Icon } from '@radix-ui/react-icons';
+import Link from "next/link";
 
 interface Props {
    params: { id: string } //
@@ -29,16 +32,27 @@ const IssueDetailPage = async ({params }: Props) => {
 
    // otherwise
    return (
-      <div>
-         <Heading>{issue.title}</Heading>
-         <Flex gap="2" my="2"> {/* gap: space between & my: margin vertical = both Radix props or keys */}
-            <IssueStatusBadge status={issue.status} />
-            <Text>{ issue.createdAt.toDateString() }</Text>
-         </Flex>
-         <Card className="prose" mt="4"> {/* add Tailwind "prose" & Radix mt:top margin */}
-            <ReactMarkdown>{ issue.description }</ReactMarkdown>
-         </Card>
-      </div>
+      <Grid columns={{initial:"1", md:"2"}} gap="5">
+         {/* Left child column */}
+         <Box>
+            <Heading>{issue.title}</Heading>
+            <Flex gap="2" my="2"> {/* gap: space between & my: margin vertical = both Radix props or keys */}
+               <IssueStatusBadge status={issue.status} />
+               <Text>{ issue.createdAt.toDateString() }</Text>
+            </Flex>
+            <Card className="prose" mt="4"> {/* add Tailwind "prose" & Radix mt:top margin */}
+               <ReactMarkdown>{ issue.description }</ReactMarkdown>
+            </Card>
+         </Box>
+         {/* Right child column */}
+         <Box>
+            <Button>
+               <Pencil2Icon />
+               <Link href={`/issues/${issue.id}/edit`}>Edit Issue</Link>
+            </Button>
+         </Box>
+
+      </Grid>
    )
 }
 
