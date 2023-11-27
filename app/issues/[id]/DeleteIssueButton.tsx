dce06048b7de14606ccde35ec5d231e54ba74ca1
Rolando L Gonzales <rolandolloyd@gmail.com>
2023-11-27 05:37:25 -0500

    Show a confirmation dialog box

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,4 +1,6 @@
-import { Button } from '@radix-ui/themes';
+'use client';
+
+import { AlertDialog, Button, Flex } from '@radix-ui/themes';
 import { GoTrash } from "react-icons/go";
 
 interface Props {
@@ -7,11 +9,33 @@ interface Props {
 
 const DeleteIssueButton = ( {issueId}: Props ) => {
   return (
-    <Button color='red'>
-      {/* <GoTrash className="md:hidden"/> */}
-      <GoTrash />
-      Delete Issue
-   </Button>
+   <AlertDialog.Root>
+
+      {/* Button trigger */}
+      <AlertDialog.Trigger>
+         <Button color='red'>
+            <GoTrash />Delete Issue
+         </Button>
+      </AlertDialog.Trigger>
+
+      {/* text inside content */}
+      <AlertDialog.Content>
+         <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
+         <AlertDialog.Description>Are you sure you want to delete this issue? This action cannot be undone.</AlertDialog.Description>
+
+         {/* Dialog buttons */}
+         <Flex mt="4" gap="3">
+            <AlertDialog.Cancel>
+               <Button variant='soft' color='gray'>Cancel</Button>
+            </AlertDialog.Cancel>
+            <AlertDialog.Action>
+               <Button color='red'>Delete</Button>
+            </AlertDialog.Action>
+         </Flex>
+
+      </AlertDialog.Content>
+
+   </AlertDialog.Root>
   )
 }
 
