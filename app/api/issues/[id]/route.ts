10dcfdf07735f3c1b8163b00da508385968c2ed5
Rolando L Gonzales <rolandolloyd@gmail.com>
2023-11-27 06:37:28 -0500

    Build an API for deleting issues

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -7,38 +7,55 @@ interface Props {
    params: { params: { id: string }}
 }
 
-// export function PATCH(request: NextRequest, { params }: Props); //using the Props interface
-export async function PATCH(
-   request: NextRequest,
-   { params }: { params: { id: string }}) { //Props is a concept tie so much with with React. We'll use an inline annotations
-      const body = await request.json(); //to read the body of the request
+// export async function PATCH(request: NextRequest, { params: {params} }: Props){ //using the Props interface
+export async function PATCH(request: NextRequest, { params }: { params: { id: string }}) { //Props is a concept tie so much with with React. We'll use an inline annotations
 
-      //validate the body using the "validationSchemas.ts" with the name "createIssueSchema" which we'll re-name to "issueSchema" to be more generic
-      const validation = issueSchema.safeParse(body);
+   const body = await request.json(); //to read the body of the request
 
-      //validation un-successfull
-      if(!validation.success)
-         return NextResponse.json(validation.error.format(), { status: 400 });
+   //validate the body using the "validationSchemas.ts" with the name "createIssueSchema" which we'll re-name to "issueSchema" to be more generic
+   const validation = issueSchema.safeParse(body);
 
-      //otherwise
-      const issue = await prisma.issue.findUnique({
-         where: { id: parseInt(params.id) }
-      });
+   //validation un-successfull
+   if(!validation.success)
+      return NextResponse.json(validation.error.format(), { status: 400 });
 
-      //if there's no issue
-      if(!issue)
-         return NextResponse.json({ error: 'Invalid issue'}, { status: 404 }); //not found error
+   //otherwise
+   const issue = await prisma.issue.findUnique({
+      where: { id: parseInt(params.id) }
+   });
 
-      //otherwise update the title & description
-      const updatedIssue = await prisma.issue.update({
-         where: { id: issue.id },
-         data: {
-            title: body.title,
-            description: body.description
-         }
-      })
+   //if there's no issue
+   if(!issue)
+      return NextResponse.json({ error: 'Invalid issue'}, { status: 404 }); //not found error
 
-      //return results to the client
-      return NextResponse.json(updatedIssue);
+   //otherwise update the title & description
+   const updatedIssue = await prisma.issue.update({
+      where: { id: issue.id },
+      data: {
+         title: body.title,
+         description: body.description
+      }
+   })
 
-   }
+   //return results to the client
+   return NextResponse.json(updatedIssue);
+
+}
+
+/*-= Deleting an Issue =-*/
+// export async function DELETE(request: NextRequest, { params: {params} }: Props){ //using the Props interface
+export async function DELETE(request: NextRequest, { params }: { params: { id: string }}) {
+
+   const issue = await prisma.issue.findUnique({
+      where: { id: parseInt(params.id) }
+   })
+
+   if(!issue) //if there's no issue
+      return NextResponse.json({ error: "Invalid Issue"}, {status: 404})
+
+   await prisma.issue.delete({ //if issue exist, delete
+      where: { id: issue.id }
+   });
+
+   return NextResponse.json({});
+}
