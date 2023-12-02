8a7669d14ddd9ace0b36b5d1450e442d6fdf575d
Rolando L Gonzales <rolandolloyd@gmail.com>
2023-12-02 06:25:20 -0500

    Set up React Query

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,11 +5,12 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Container, Theme, ThemePanel } from "@radix-ui/themes";
 import NavBar from "./NavBar";
-import AuthProvider from './auth/Provider';
+import AuthProvider from "./auth/Provider";
+import QueryClientProvider from "./QueryClientProvider";
 
 const inter = Inter({
-   subsets: ["latin"],
-   variable: "--font-inter"
+	subsets: ["latin"],
+	variable: "--font-inter",
 });
 
 export const metadata: Metadata = {
@@ -21,18 +22,18 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 	return (
 		<html lang="en">
 			<body className={inter.variable}>
-            <AuthProvider>
-               <Theme accentColor="green" radius="medium">
-                  <NavBar />
-                  <main className="p-5">
-                     <Container>
-                        {children}
-                     </Container>
-                  </main>
-                  {/* Theme styling panel */}
-                  {/* <ThemePanel /> */}
-               </Theme>
-            </AuthProvider>
+				<QueryClientProvider>
+					<AuthProvider>
+						<Theme accentColor="green" radius="medium">
+							<NavBar />
+							<main className="p-5">
+								<Container>{children}</Container>
+							</main>
+							{/* Theme styling panel */}
+							{/* <ThemePanel /> */}
+						</Theme>
+					</AuthProvider>
+				</QueryClientProvider>
 			</body>
 		</html>
 	);
