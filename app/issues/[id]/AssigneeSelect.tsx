d2b53ce0de0f707a05113c28bc5691486d410b8f
Rolando L Gonzales <rolandolloyd@gmail.com>
2023-12-03 06:42:28 -0500

    Fetch data with React Query

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,19 +1,38 @@
 "use client";
 
+import { Skeleton } from '@/app/components';
 import { User } from '@prisma/client';
 import { Select } from '@radix-ui/themes'
+import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 
 const AssigneeSelect = () => {
-   const [users, setUsers] = useState<User[]>([]); //type of our users, Prisma User
-   useEffect(() =>  { // useEffect hook to call the backend (callback). for more details using Hooks checkout React course, calling backends
-      const fetchUsers = async () => {
-         const {data} = await axios.get<User[]>('/api/users');
-         setUsers(data);
-      }
-      fetchUsers();
-   }, [])
+   // using tanStack reactQuery useQuery hook
+   const { data: users, error, isLoading } = useQuery<User[]>({
+      queryKey: ['users'],
+      queryFn: () => axios.get('/api/users').then(res => res.data), //this return a Promise that resolved to data
+      staleTime: 60 * 1000, //useQuery re-fetch timer
+      retry: 3 // useQuery retry fetching 3 times
+   });
+   if(isLoading) {
+      return (
+         <Skeleton />
+      )
+   };
+   //if(isLoading) return <Skeleton /> //single line
+
+   if(error) return null;
+
+   // useQuery will replace the ff:
+   // const [users, setUsers] = useState<User[]>([]); //type of our users, Prisma User
+   // useEffect(() =>  { // useEffect hook to call the backend (callback). for more details using Hooks checkout React course, calling backends
+   //    const fetchUsers = async () => {
+   //       const {data} = await axios.get<User[]>('/api/users');
+   //       setUsers(data);
+   //    }
+   //    fetchUsers();
+   // }, [])
 
   return (
     <Select.Root>
@@ -23,7 +42,7 @@ const AssigneeSelect = () => {
          <Select.Group>
             <Select.Label>Suggestions</Select.Label>
 
-            {users.map(user => (
+            {users?.map(user => ( //use optional chaining 'coz initially 'users' is undefined until data is fetched from the backend
                <Select.Item key={user.id} value={user.id}>{user.name}</Select.Item>
             ))}
 
